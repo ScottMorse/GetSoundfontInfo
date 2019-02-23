75f0d1f6e6a6d72017492bcd0401940bfe228bfa
Scott Morse <scottmorsedev@gmail.com>
2019-02-23 10:37:23 -0600

    Function for reading binary of samples

--- parse_soundfont.py
+++ parse_soundfont.py
@@ -12,14 +12,18 @@ with open(FILE_PATH,'rb') as f:
 with open(FILE_PATH,'rb') as f:
     sf2 = Sf2File(f)
 
-def read_range(start,end,file=FILE_PATH):
+SAMPLE_OFFSET = sf2.raw.smpl_offset
+
+def read_sample_data(bytes,start=0,offset=SAMPLE_OFFSET,file=FILE_PATH):
     with open(file,'rb') as f:
-        f.seek(start)
-        return f.read(end - start)
+        f.seek(offset + start)
+        return f.read(bytes)
 
 data = sf2.raw.pdta
 phdr = data['Phdr']
 shdr = data['Shdr']
 
 #grand pianos: 96-98
-grand_piano_data = phdr[98] 
+grand_piano_data = phdr[98]
+
+print(read_sample_data(100))
