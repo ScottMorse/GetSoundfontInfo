ad211310c17f17c2f71962546c4ad77a9b652ee3
Scott Morse <scottmorsedev@gmail.com>
2019-02-23 10:29:15 -0600

    Getting somewhere with the file reading

--- parse_soundfont.py
+++ parse_soundfont.py
@@ -1,11 +1,25 @@
 from sf2utils.sf2parse import Sf2File
+import sf2utils
 from pprint import pprint
 from pyaudio import PyAudio
+import pymusician as pm
 
 FILE_PATH = "../../Documents/niceKeys23.sf2"
 
 with open(FILE_PATH,'rb') as f:
+    binary = f.read(10)
+
+with open(FILE_PATH,'rb') as f:
     sf2 = Sf2File(f)
 
-pprint(sf2.raw.pdta['Phdr'][1])
-pprint(sf2.raw.pdta['Shdr'][24])
+def read_range(start,end,file=FILE_PATH):
+    with open(file,'rb') as f:
+        f.seek(start)
+        return f.read(end - start)
+
+data = sf2.raw.pdta
+phdr = data['Phdr']
+shdr = data['Shdr']
+
+#grand pianos: 96-98
+grand_piano_data = phdr[98] 
